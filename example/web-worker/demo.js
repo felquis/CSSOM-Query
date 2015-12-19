9306faa2abf184b03245164875b8220c4ac57b4d
Felquis Gimenes <frgformenton@gmail.com>
2015-12-19 09:21:11 -0200

    Remove ;

--- example/web-worker/demo.js
+++ example/web-worker/demo.js
@@ -1,28 +1,27 @@
-var myWorker = null,
-  URL = window.URL || (window.webkitURL);
-
-window.URL = URL;
+var myWorker = null
+window.URL = window.URL || (window.webkitURL)
 
 function workForMe() {
-  var i = 0;
+  var i = 0
+
   setInterval(function() {
-    i++;
-    postMessage(i);
-  }, 500);
+    i++
+    postMessage(i)
+  }, 500)
 }
 
 function useFuncInWorker(func) {
   var file = '(' + func.toString() + ')()'
 
   var blob = new Blob([file], {
-    type: "text/javascript"
-  });
+    type: 'text/javascript'
+  })
 
-  return new Worker(window.URL.createObjectURL(blob));
+  return new Worker(window.URL.createObjectURL(blob))
 }
 
 var myWorker = useFuncInWorker(workForMe)
 
 myWorker.onmessage = function (e) {
-  document.getElementById('counter').innerHTML = e.data;
-};
+  document.getElementById('counter').innerHTML = e.data
+}
