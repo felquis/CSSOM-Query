f42c7d3a0695f217a7785fe79ff47eb8ee4ced00
Felquis Gimenes <frgformenton@gmail.com>
2015-12-19 00:45:08 -0200

    Check if the last property is length
      This is a try to workaround a Safari bug
      now the demos works on Safari 9 tested on OSX 10.10
    
      closes #1

--- index.js
+++ index.js
@@ -54,8 +54,18 @@
     Utility, iterate Objects, Lists, Arrays..
   */
   function each(list, transform) {
-    var keys = Object.keys(list)
-    var index = 0, total = keys.length
+    var keys = Object.keys(list),
+        index = 0,
+        total
+
+    // Check if 'length' is the last key
+    // this is a guess to turn around this Safari bug
+    // https://bugs.webkit.org/show_bug.cgi?id=152454
+    if (keys.indexOf('length') === (keys.length - 1)) {
+      keys.pop()
+    }
+
+    total = keys.length
 
     if (typeof transform !== 'function') {
       throw('The second param must be a function')
